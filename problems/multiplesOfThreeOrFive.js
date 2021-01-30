6f213660ce52324fad5448d5e2a4fdc80a4a3afe
jcanizales23 <johncanizales@pursuit.org>
2021-01-30 15:31:56 -0500

    Tried one more time before I moved on

--- problems/multiplesOfThreeOrFive.js
+++ problems/multiplesOfThreeOrFive.js
@@ -25,12 +25,14 @@
 */
 
 function multiplesOfThreeOrFive(n) {
-    let sum = 0;
+    let sum = n > 0;
+        sum = n < 10
     for (let i = 0; i <= n; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
             sum += i;
         }
     }
+    return n;
 }
 
 module.exports = multiplesOfThreeOrFive
