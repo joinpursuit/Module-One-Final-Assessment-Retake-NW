860bd42ca78044d882561903a996db9779b31517
Jon Narine <jonnicwolf@gmail.com>
2021-01-30 18:02:55 -0500

    didnt realise I missed totalNumberOfStudents dangg it

--- problems/totalNumberOfStudents.js
+++ problems/totalNumberOfStudents.js
@@ -12,7 +12,16 @@
 */
 
 function totalNumberOfStudents(arr) {
-    
+    const neuArr = [];
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        neuArr.push(arr[i])
+       count = count + arr[i]
+    }
+    console.log(neuArr)
+    return neuArr
+
 }
+console.log(totalNumberOfStudents([{subject: "A", studentCount: 10}, {subject: "B", studentCount: 100}]))
 
 module.exports = totalNumberOfStudents
