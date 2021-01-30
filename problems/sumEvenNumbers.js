34c8f84436324de62e6ddde60f3eacfbcdd2c52d
jcanizales23 <johncanizales@pursuit.org>
2021-01-30 16:15:45 -0500

    So close

--- problems/sumEvenNumbers.js
+++ problems/sumEvenNumbers.js
@@ -10,7 +10,14 @@
 * returns: 14
 */
 function sumEvenNumbers() {
-
+    let arr = [];
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(i % 2 === 0) {
+            sum = sum + arr[i];
+        }
+    }
+    return sum;
 }
 
 module.exports = sumEvenNumbers
